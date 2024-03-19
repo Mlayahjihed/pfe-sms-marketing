0e7bcbdfecc813b342a24da925c7dd06cdb3cf0d
Mlayahjihed <94804592+Mlayahjihed@users.noreply.github.com>
2024-03-19 02:30:11 +0100

    Update planfiesms.js
    ss2

--- controllers/planfiesms.js
+++ controllers/planfiesms.js
@@ -3,7 +3,7 @@ const schedule = require('node-schedule');
 var Axios =require('axios') ;
 exports.planfie = async ({date,entet,msg,num}) => {
     var j = schedule.scheduleJob(date,async function(){
-        var url="https://swatek.tn/apisms/sendsms.php?tel=xxxxxxxx&entete=Test1&message=Test2";
+        var url="api sms";
         url =url.replace("xxxxxxxx",num);
         url =url.replace("Test1",entet);
         url =url.replace("Test2",msg);
